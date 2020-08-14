d3d4cb82de153c06b159eb2747ca614251070f2c
gaiyadev <gaiyaobed94@gmail.com>
2020-08-14 03:24:17 +0100

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:chat_app/screens/chat_screen.dart';
+import 'package:chat_app/screens/login_screen.dart';
+import 'package:chat_app/screens/registration_screen.dart';
+import 'package:chat_app/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: RegistrationScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+      },
+    );
+  }
+}
