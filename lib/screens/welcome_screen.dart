91fdd71b9f1abe35ace5173aac61fc878df8e64a
gaiyadev <gaiyaobed94@gmail.com>
2020-08-15 21:20:49 +0100

    Refactor: refactor code in the login and register screen to use a one btn

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -2,6 +2,7 @@ import 'package:chat_app/screens/login_screen.dart';
 import 'package:chat_app/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:chat_app/components/rounded_button.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
